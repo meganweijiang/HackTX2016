fc123bcf624577731b6fef9eaa387004e7e358b9
meganweijiang <meganweijiang@utexas.edu>
2016-10-24 15:47:16 -0500

    Add files

---
+++ Room Simulation/Assets/CatAI.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatAI : MonoBehaviour {
+
+	public float speed = 4.0f;
+	private bool isCollided = false;
+	private Rigidbody cat;
+	private int catDirection; 
+
+	// Use this for initialization
+	void Start () {
+		cat = GetComponent<Rigidbody>();		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		cat.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+		transform.position += transform.forward*speed*Time.deltaTime;
+
+		speed += 0.1f;
+
+		if (speed > 8.0f) {
+			speed = 4.0f;
+		}
+
+		if (isCollided) {
+			transform.Rotate(0, 15, 0);
+		}
+	}
+
+	void OnCollisionEnter(Collision col){
+		if(col.gameObject.name == "Wall1" || col.gameObject.name == "Wall2" || col.gameObject.name == "Wall3" || col.gameObject.name == "Wall4")
+		{
+			isCollided = true;
+		}
+	}
+
+	void OnCollisionExit(Collision col){
+		if(col.gameObject.name == "Wall1" || col.gameObject.name == "Wall2" || col.gameObject.name == "Wall3" || col.gameObject.name == "Wall4")
+		{
+			isCollided = false;
+		}
+	}
+}
---
+++ Room Simulation/Assets/PlayerControl.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System;
+
+public class PlayerControl : MonoBehaviour {
+
+	private Rigidbody thing;
+	public float speed = 8.0f;
+	private bool isGrounded = true;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		thing = GetComponent<Rigidbody>();
+	}
+
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			Application.Quit();
+		}
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (isMoving()) {
+			thing.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+			float rotateY = Input.GetAxis ("Horizontal");
+			float moveZ = Input.GetAxis ("Vertical");
+
+			transform.Rotate(0, rotateY, 0);
+
+			Vector3 move = new Vector3 (0.0f, 0.0f, moveZ);
+			move *= speed;
+			transform.Translate(move*Time.deltaTime);
+
+			if (Input.GetButtonDown ("Jump") && isGrounded) {
+				Vector3 moveJump = new Vector3 (0.0f, 300.0f, moveZ);
+				thing.AddForce(moveJump);
+			}
+		} 
+
+		else {
+			thing.constraints = RigidbodyConstraints.FreezeRotation;
+		}
+			
+	}
+
+	bool isMoving () {
+		bool moving = false;
+		if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0 || Input.GetButtonDown ("Jump")) {
+			moving = true;
+		}
+		return moving;
+	}
+
+	void OnCollisionEnter(Collision col){
+		if(col.gameObject.name == "Plane")
+		{
+			isGrounded = true;
+		}
+	}
+
+	void OnCollisionExit(Collision col){
+		if(col.gameObject.name == "Plane")
+		{
+			isGrounded = false;
+		}
+	}
+}
